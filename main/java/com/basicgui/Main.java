db0e761dad62e0f8802c1f6089b24242edc7019d
Lucas <lukgammer.amaral@gmail.com>
2025-09-28 18:21:02 -0300

    tentativa errada

--- main/java/com/basicgui/Main.java
+++ main/java/com/basicgui/Main.java
@@ -6,7 +6,6 @@ import com.presentation.controller.MainController;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            // Cria e exibe a janela principal
             MainController mainFrame = new MainController();
             mainFrame.setVisible(true);
         });
